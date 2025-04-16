404d4f0df350444346215f0df8f6938335fb5e99
andrestrepo <andres.restrepo@mercadolibre.com.co>
2025-04-15 20:45:54 -0500

    feat: add prompts from files
    Signed-off-by: andrestrepo <andres.restrepo@mercadolibre.com.co>

--- src/app/llm/llm.py
+++ src/app/llm/llm.py
@@ -1,3 +1,4 @@
+import os
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from typing import Optional, List, Dict
@@ -7,8 +8,6 @@ from pydantic import BaseModel, Field
 from langchain_core.output_parsers import PydanticOutputParser
 
 from langchain_core.prompts import (
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
     ChatPromptTemplate,
     PromptTemplate,
 )
@@ -66,46 +65,31 @@ class Llm:
     def get_llm(self):
         return self.llm
 
+    def load_prompt(self, prompt_path: str):
+        if not os.path.exists(prompt_path):
+            raise FileNotFoundError(f"Prompt no encontrado: {prompt_path}")
+        with open(prompt_path) as f:
+            prompt_text = f.read()
+        return prompt_text
+
 
 class SummaryLlm(Llm):
     def __init__(self, config: dict = default_config):
         super().__init__(config)
         self.parser = PydanticOutputParser(pydantic_object=ExtractAssets)
 
-        self.summary_system_prompt = SystemMessagePromptTemplate.from_template(
-            """Eres un experto de trading y analisis de mercado. Provee resumenes precisos y consisos."""
+        self.summary_prompt_template = self.load_prompt(
+            prompt_path="app/prompt/v1_summary_asistance.txt"
         )
-        self.summary_prompt_template = """
-            **Tarea:** Resume y elaborar un informe con los topicos mas importantes del siguiente texto.
-
-            **Texto:**
-            {context}
-
-            **Instrucciones:**
-            Comience el resumen con una "Introducción" que ofrezca una visión general del tema seguido por los puntos más importantes ("Bullet Points"). Termina el resumen con una conclusión.
-            **Además, extrae los activos mencionados para invertir y crea una lista separada de los mismos.**
-
-            **Resumen:**
-        """
-        self.summary_prompt = HumanMessagePromptTemplate.from_template(
-            self.summary_prompt_template
+        self.extract_info_prompt_template = self.load_prompt(
+            prompt_path="app/prompt/v1_extract_info.txt"
         )
-
-        self.extract_info_system_prompt = SystemMessagePromptTemplate.from_template(
-            """Eres un experto de trading y análisis de mercado. Provee respuestas precisas y concisas."""
-        )
-        self.extract_info_prompt_template = """
-            Eres un experto de trading y análisis de mercado.
-            **Tarea:** Extrae los activos mencionados en el siguiente texto.
-            **Texto:**
-            {context}
-            **Instrucciones:**
-            Extrae de forma concisa los activos mencionados para invertir.
-            {format_instruction}"""
 
     def summarize(self, context):
-        messages = [self.summary_system_prompt, self.summary_prompt]
-        template = ChatPromptTemplate(messages)
+        template = PromptTemplate(
+            template=self.summary_prompt_template,
+            input_variables=["context"],
+        )
 
         qna_chain = template | self.llm | StrOutputParser()
         return qna_chain.invoke({"context": context})
@@ -120,3 +104,17 @@ class SummaryLlm(Llm):
         )
         qna_chain = template | self.llm | self.parser
         return qna_chain.invoke({"context": context})
+
+
+class ChatLlm(Llm):
+    def __init__(self, config: dict = default_config):
+        super().__init__(config)
+        self.prompt_template = Llm.load_prompt(
+            prompt_path="app/prompt/v1_expert_asistance.txt"
+        )
+
+    def ask_llm(self, context, question):
+        template = ChatPromptTemplate(template_format=self.prompt_template)
+
+        qna_chain = template | self.llm | StrOutputParser()
+        return qna_chain.invoke({"context": context, "question": question})
